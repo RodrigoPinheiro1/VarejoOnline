96a260e7fe37db21562d44d2ee7f46f2c1737e6a
rodrigo pinheiro <rodrigopsps2015@gmail.com>
2023-05-25 16:15:41 -0300

    Adicionado Flyway para base de dados

--- server/src/main/java/br/comvarejonline/projetoinicial/Service/implem/GerenteServiceimplemts.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/Service/implem/GerenteServiceimplemts.java
@@ -1,9 +1,10 @@
 package br.comvarejonline.projetoinicial.Service.implem;
 
-import br.comvarejonline.projetoinicial.dto.EstoqueDto;
+import br.comvarejonline.projetoinicial.dto.GerenteCadastroDto;
 import br.comvarejonline.projetoinicial.dto.GerenteDto;
 import br.comvarejonline.projetoinicial.model.Gerente;
 import br.comvarejonline.projetoinicial.model.Perfil;
+import br.comvarejonline.projetoinicial.model.Usuario;
 import br.comvarejonline.projetoinicial.repository.GerenteRepository;
 import br.comvarejonline.projetoinicial.repository.PerfilRepository;
 import org.modelmapper.ModelMapper;
@@ -11,8 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 public class GerenteServiceimplemts {
@@ -26,15 +26,18 @@ public class GerenteServiceimplemts {
     private PerfilRepository perfilRepository;
 
 
-    public GerenteDto cadastrarGerente(GerenteDto dto) {
+    public GerenteDto cadastrarGerente(GerenteCadastroDto dto) {
 
         Gerente gerente = modelMapper.map(dto, Gerente.class);
-
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        Perfil perfil = perfilRepository.getById(dto.getPerfilId());
+
         String encode = bCryptPasswordEncoder.encode(gerente.getSenha());
         gerente.setSenha(encode);
+        gerente.setPerfil(Collections.singletonList(perfil));
 
-        gerente.getPerfil().forEach(perfil -> perfil.setNome("GERENTE"));
+
         gerenteRepository.save(gerente);
 
         return modelMapper.map(gerente, GerenteDto.class);
--- server/src/main/java/br/comvarejonline/projetoinicial/Service/implem/OperadorServiceimplemts.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/Service/implem/OperadorServiceimplemts.java
@@ -1,14 +1,17 @@
 package br.comvarejonline.projetoinicial.Service.implem;
 
-import br.comvarejonline.projetoinicial.dto.GerenteDto;
+import br.comvarejonline.projetoinicial.dto.CadastroOperadorDto;
 import br.comvarejonline.projetoinicial.dto.OperadorDto;
 import br.comvarejonline.projetoinicial.model.Operador;
-import br.comvarejonline.projetoinicial.repository.GerenteRepository;
+import br.comvarejonline.projetoinicial.model.Perfil;
 import br.comvarejonline.projetoinicial.repository.OperadorRepository;
+import br.comvarejonline.projetoinicial.repository.PerfilRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class OperadorServiceimplemts {
@@ -18,17 +21,26 @@ public class OperadorServiceimplemts {
     @Autowired
     private OperadorRepository operadorRepository;
 
-    public OperadorDto cadastrarOperador(OperadorDto operadorDto) {
+    @Autowired
+    private PerfilRepository perfilRepository;
 
-        Operador operador = modelMapper.map(operadorDto, Operador.class);
+
+    public OperadorDto cadastrarOperador(CadastroOperadorDto dto) {
+
+        Operador operador = modelMapper.map(dto, Operador.class);
+
+        Perfil perfil = perfilRepository.getById(dto.getPerfilId());
 
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
         String encode = bCryptPasswordEncoder.encode(operador.getSenha());
         operador.setSenha(encode);
+        operador.setPerfil(Collections.singletonList(perfil));
+
 
         operadorRepository.save(operador);
 
-        return modelMapper.map(operador,OperadorDto.class);
+        return modelMapper.map(operador, OperadorDto.class);
 
     }
 }
