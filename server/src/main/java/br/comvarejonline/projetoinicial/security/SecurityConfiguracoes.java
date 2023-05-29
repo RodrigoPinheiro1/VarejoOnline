c3b2e2bd043fc9ed8f9d25d2af7636fdee7f74ed
rodrigo pinheiro <rodrigopsps2015@gmail.com>
2023-05-29 14:13:33 -0300

    colocado variaveis para permissoes

--- server/src/main/java/br/comvarejonline/projetoinicial/security/SecurityConfiguracoes.java
+++ server/src/main/java/br/comvarejonline/projetoinicial/security/SecurityConfiguracoes.java
@@ -4,13 +4,10 @@ import br.comvarejonline.projetoinicial.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -47,10 +44,10 @@ public class SecurityConfiguracoes {
                 .antMatchers(HttpMethod.GET,"/login").permitAll()
                 .antMatchers(HttpMethod.POST,"/gerente").permitAll()
                 .antMatchers(HttpMethod.POST,"/operador").permitAll()
-               .antMatchers(HttpMethod.POST,"/produto").hasAuthority("GERENTE")
-                .antMatchers(HttpMethod.PUT,"/produto").hasAuthority("GERENTE")
-                .antMatchers(HttpMethod.POST,"/estoque").hasAnyAuthority("GERENTE","OPERADOR")
-                .antMatchers(HttpMethod.PUT,"/estoque").hasAnyAuthority("GERENTE","OPERADOR")
+               .antMatchers(HttpMethod.POST,"/produto").hasAuthority(gerente)
+                .antMatchers(HttpMethod.PUT,"/produto").hasAuthority(gerente)
+                .antMatchers(HttpMethod.POST,"/estoque").hasAnyAuthority(gerente,operador)
+                .antMatchers(HttpMethod.PUT,"/estoque").hasAnyAuthority(gerente,operador)
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
